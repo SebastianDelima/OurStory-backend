efb00610404f02d82753ecb4194a9ecf2e180228
Sebastian Delima <sebastiandelima@Cactus1.local>
2020-02-07 20:43:55 -0500

    good Db

--- app/controllers/user_stories_controller.rb
+++ app/controllers/user_stories_controller.rb
@@ -1,5 +1,10 @@
 class UserStoriesController < ApplicationController
 
+    def index
+        user_stories = UserStory.all
+        render :json => user_stories
+    end
+
     def create
         user_story = UserStory.create(user_story_params)
         render :json => user_story
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Story.destroy_all
+
+
 
 users = [{name: 'matt',   email:'helo@gamil.com', shortBio:'hello man'}, {name: 'Zara',  email:'helo@gamil.com', shortBio:'hello man'},
          {name: 'george', email:'helo@gamil.com', shortBio:'hello man'}, {name: 'Chine', email:'helo@gamil.com', shortBio:'hello man'}, 
@@ -19,11 +20,3 @@ users = [{name: 'matt',   email:'helo@gamil.com', shortBio:'hello man'}, {name: 
         }
 
 
-stories = [{title:"A funny story",         image:'https://bit.ly/31fJUrv', description:"Read this and you will die in the next year", completed:true}, {title:"Junior Boy",       image:'https://bit.ly/2RAgNf0', description:"Read this and you will die in the next year", completed:true}, {title:"White house",  image:'https://bit.ly/2S3krwS', description:"Read this and you will die in the next year", completed:false},
-           {title:"The Hence in the park", image:'https://bit.ly/2O8HRA4', description:"Read this and you will die in the next year", completed:true}, {title:"My friend arnold", image:'https://bit.ly/2S3krwS', description:"Read this and you will die in the next year", completed:true}, {title:"Black House",  image:'https://bit.ly/31fJUrv', description:"Read this and you will die in the next year", completed:false},
-           {title:"Black Mamba",           image:'https://bit.ly/2uG2A7o', description:"Read this and you will die in the next year", completed:true}, {title:"Chek in",          image:'https://bit.ly/2S3krwS', description:"Read this and you will die in the next year", completed:true}, {title:"Yellow House", image:'https://bit.ly/2S3krwS', description:"Read this and you will die in the next year", completed:false},
-           {title:"Little Boys",           image:'https://bit.ly/2OpziRF', description:"Read this and you will die in the next year", completed:true}, {title:"give me money",    image:'https://bit.ly/31fJUrv', description:"Read this and you will die in the next year", completed:true}, {title:"Image House",  image:'https://bit.ly/2S3krwS', description:"Read this and you will die in the next year", completed:false}]
-
-         stories.map { |story|
-            Story.create(title: story[:title], image: story[:image], description: story[:description], completed: story[:completed])
-        }
